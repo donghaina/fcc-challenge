9b697fd565f6e7164c1709832b8213451fa7410c
taosang1992 <taosang1992@gmail.com>
2017-04-19 23:25:47 +0800

    update

--- Show-the-Local-Weather/script.js
+++ Show-the-Local-Weather/script.js
@@ -1,16 +1,34 @@
 $(document).ready(function() {
-    var lat, lon;
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            lat = position.coords.latitude;
-            lon = position.coords.longitude;
-            console.log(lat);
-            console.log(lon);
-        });
-    }
+    // var lat, lon;
+    // var arr = [];
+    // if (navigator.geolocation) {
+    //     navigator.geolocation.getCurrentPosition(function(position) {
+    //         lat = Math.round(position.coords.latitude);
+    //         lon = Math.round(position.coords.longitude);
+    //         var api = 'http://api.openweathermap.org/data/2.5/weather?' + "lat=" + lat + "&lon=" + lon + "&appid=86e8744e87c5dc3b51370144a2d7df8b";
+    //         console.log(api);
+    //         $.getJSON(api, function(data) {
+    //             document.write("城市: " + data.name + "<br>" + "温度：" + data.main.temp);
+    //         });
+    //     });
+    // }
 
-
-    $.getJSON('http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=86e8744e87c5dc3b51370144a2d7df8b', function(data) {
-        document.write("Name: " + data.name + "<br>" + "Cod: " + data.cod);
+    // $.getJSON('http://ipinfo.io', function(location) {
+    //     console.log(location.city);
+    //     // console.log(typeof location.loc);
+    //     var locArr = location.loc.split(',');
+    //     console.log(locArr);
+    //     var lat = locArr[0];
+    //     var lon = locArr[1];
+    //     // console.log(lat+","+lon);
+    //     var api = 'http://api.openweathermap.org/data/2.5/weather?' + "lat=" + lat + "&lon=" + lon + "&appid=86e8744e87c5dc3b51370144a2d7df8b";
+    //     console.log(api);
+    //     $.getJSON(api,function(data){
+    //     	document.write("城市: " + location.city + "<br>" + "温度：" + data.main.temp);
+    //     });
+    // });
+    
+    $.getJSON('http://api.openweathermap.org/data/2.5/weather?id=1795565&appid=86e8744e87c5dc3b51370144a2d7df8b', function(data) {
+        console.log(data.main.temp);
     });
 });
