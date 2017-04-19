ab4c48b7a0541547e8ce4cf8367092364cda9c3d
taosang1992 <taosang1992@gmail.com>
2017-04-19 21:43:07 +0800

    update

--- Show-the-Local-Weather/script.js
+++ Show-the-Local-Weather/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
+    var lat, lon;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            lat = position.coords.latitude;
+            lon = position.coords.longitude;
+            console.log(lat);
+            console.log(lon);
+        });
+    }
+
+
     $.getJSON('http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=86e8744e87c5dc3b51370144a2d7df8b', function(data) {
-        document.write("Name: "+data.name+"<br>"+"Cod: "+data.cod);
+        document.write("Name: " + data.name + "<br>" + "Cod: " + data.cod);
     });
 });
