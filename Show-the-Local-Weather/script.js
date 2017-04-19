5bd23311e4ee516d67c97a3f46b98dea5678f89e
taosang1992 <taosang1992@gmail.com>
2017-04-19 21:26:43 +0800

    update

---
+++ Show-the-Local-Weather/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $.getJSON('http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=86e8744e87c5dc3b51370144a2d7df8b', function(data) {
+        document.write("Name: "+data.name+"<br>"+"Cod: "+data.cod);
+    });
+});
